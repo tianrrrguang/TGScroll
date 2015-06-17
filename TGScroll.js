f80a16a6101bf05dea621d30999c76ddf1914ecf
tianrrrguang <156691306@qq.com>
2015-06-17 14:49:24 +0800

    test

--- TGScroll.js
+++ TGScroll.js
@@ -2,6 +2,27 @@
 	zepto scroll plugin for mobile
 	模仿iOS滚动时间
 	当连续滚动时不会触发onscroll，只有滚动停止后才触发
+	这是分支01
+	sfasfasdsdasdaffsdadfs
+	sdf
+	sdf
+	sd
+	sdaf
+	sadg
+	fdsg
+	dfsg
+	dfsg
+	dfshfs
+	hsfg
+	hsf
+	hgfsh
+	sfg
+	hsdf
+	gsd
+	gdfa
+	gdfg
+	dfs
+	
 */
 ;(function($){
 	//check zepto
